a02b7948413f28d5e117b86b0c11b97cee9531d9
eccx400 <echeng@scu.edu>
2019-05-16 17:03:14 -0700

    Finished Problem 1

--- HW6_1/src/Problem_2.scala
+++ HW6_1/src/Problem_2.scala
@@ -2,6 +2,10 @@ object Problem_2
 {
   def main(args: Array[String])
   {
+    def apply_combine2(f:Int => Int, x:Int, g:(Int, Int) => Int) :Int = 
     
+    def apply_square = apply_combine2(x=>x*x)
+    def apply_square_5 = apply_square(5)
+    println(apply_combine2(x=>x*x)(5)(x=>x*x))
   }
 }
