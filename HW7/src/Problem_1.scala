5c4568d41efe6480e98521de2a88a67cc35d50b4
eccx400 <echeng@scu.edu>
2019-05-21 21:37:16 -0700

    Add Hw 7

---
+++ HW7/src/Problem_1.scala
@@ -0,0 +1,10 @@
+object Problem_1
+{
+  def main(args: Array[String])
+  {
+    def alternateList(x: List[Int], y: List[Int]) :List[Int]= 
+    {
+      
+    }
+  }
+}
---
+++ HW7/src/Problem_3.scala
@@ -0,0 +1,9 @@
+object Problem_3
+{
+  def main(args: Array[String])
+  {
+    def checkFunction(xs: List[Int], f:Int => Boolean): List[Int] = {
+      
+    }
+  }
+}
